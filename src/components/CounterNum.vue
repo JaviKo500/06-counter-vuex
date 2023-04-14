dc73fdfff107ba0167cbfca289247b61aa6ad533
JaViKo <javikogutierrez64@gmail.com>
2023-04-14 12:11:00 -0500

    forms access state

--- src/components/CounterNum.vue
+++ src/components/CounterNum.vue
@@ -1,12 +1,28 @@
 <template>
   <h1>Counter - vuex</h1>
   <h2>Direct Access: {{ $store.state.count }}</h2>
+  <h2>Computed: {{ countComputed }}</h2>
 
+  <h1>Map State</h1>
+  <h2>mapState: {{ count }}</h2>
+  <h2>lastMutation: {{ lastMutation }}</h2>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-    name: 'CounterNum'
+    name: 'CounterNum',
+    // computed: mapState(['count'])
+    computed: {
+      countComputed() {
+        return this.$store.state.count
+      },
+      ...mapState(['count', 'lastMutation'])
+      // ...mapState({
+      //   count: state => state.count,
+      //   lastMutation: state => state.lastMutation
+      // })
+    }
 }
 </script>
 
